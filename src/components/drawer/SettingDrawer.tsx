15ba8bfc9499a1686d34ba354a2574b014f7a76b
ayu1104 <ak3668134@gmail.com>
2025-04-06 00:57:19 +0900

    SettingDrawer内のListItemをListItemButtonに変更し型エラーを解消

--- src/components/drawer/SettingDrawer.tsx
+++ src/components/drawer/SettingDrawer.tsx
@@ -1,6 +1,13 @@
 'use client';
 
-import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material';
 import PersonIcon from '@mui/icons-material/Person';
 import PetsIcon from '@mui/icons-material/Pets';
 import LogoutIcon from '@mui/icons-material/Logout';
@@ -23,17 +30,28 @@ const SettingDrawer: React.FC<Props> = ({ open, onClose }) => {
     <Drawer anchor="right" open={open} onClose={onClose}>
       <div style={{ width: 250, padding: 16 }}>
         <List>
-          <ListItem button onClick={() => router.push('/user-profile')}>
-            <ListItemIcon><PersonIcon /></ListItemIcon>
-            <ListItemText primary="ユーザープロフィール" />
+          <ListItem disablePadding>
+            <ListItemButton onClick={() => router.push('/user-profile')}>
+              <ListItemIcon><PersonIcon /></ListItemIcon>
+              <ListItemText primary="ユーザープロフィール" />
+            </ListItemButton>
           </ListItem>
-          <ListItem button onClick={() => router.push('/dog-profile')}>
-            <ListItemIcon><PetsIcon /></ListItemIcon>
-            <ListItemText primary="わんちゃんプロフィール" />
+
+          <ListItem disablePadding>
+            <ListItemButton onClick={() => router.push('/dog-profile')}>
+              <ListItemIcon><PetsIcon /></ListItemIcon>
+              <ListItemText primary="わんちゃんプロフィール" />
+            </ListItemButton>
           </ListItem>
-          <ListItem button onClick={handleLogout}>
-            <ListItemIcon><LogoutIcon /></ListItemIcon>
-            <ListItemText primary="ログアウト" primaryTypographyProps={{ fontWeight: 'bold' }} />
+
+          <ListItem disablePadding>
+            <ListItemButton onClick={handleLogout}>
+              <ListItemIcon><LogoutIcon /></ListItemIcon>
+              <ListItemText
+                primary="ログアウト"
+                primaryTypographyProps={{ fontWeight: 'bold' }}
+              />
+            </ListItemButton>
           </ListItem>
         </List>
       </div>
